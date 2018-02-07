8fcd8e34e1b4d3c2c0a8f4a640410b4477c2af00
J. Eurique Cardoso Ribeiro Junior <jecrjunior@dcomp.ufs.br>
2018-02-07 12:04:26 -0300

    Money

--- lib/money.ex
+++ lib/money.ex
@@ -1,23 +1,35 @@
 defmodule Money do
-    defp normalize({mantissa, exponent}, to_exponent) do
-        
-    end
-
+    alias Decimal, as: D
     def add(value1, value2) do
+        sum = D.add(D.new(value1), D.new(value2))
+        {D.to_float(sum)}
     end
 
     def sub(value1, value2) do
+        sub = D.sub(D.new(value1), D.new(value2))
+        {D.to_float(sub)}
     end
 
     def mul(value1, value2) do
+        mul = D.mult(D.new(value1), D.new(value2))
+        {D.to_float(mul)}
     end
 
     def div(value1, value2) do
+        div = D.div(D.new(value1), D.new(value2))
+        {D.to_float(div)}
     end
 
-    def lower_or_equal_than(value1, value2) do     
+    def lower_or_equal_than(value1, value2) do   
+        div = D.div(D.new(value1), D.new(value2))
+        {D.to_float(div)}  
     end
 
     def greater_or_equal_than(value1, value2) do
+        div = D.div(D.new(value1), D.new(value2))
+        {D.to_float(div)}
     end
 end
+
+{m} = Money.div(10.23, 8.7383)
+ IO.puts m 
